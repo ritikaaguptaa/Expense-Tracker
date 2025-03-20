97ad14e12b573662cf90704b8f990cf54114f1ff
ritikaaguptaa <ritika@korecent.com>
2025-03-20 16:51:02 +0530

    telegram notification

--- expense_tracker/expense_tracker/doctype/family_member/family_member.py
+++ expense_tracker/expense_tracker/doctype/family_member/family_member.py
@@ -1,12 +1,8 @@
-# Copyright (c) 2025, siddharth and contributors
-# For license information, please see license.txt
-
-# import frappe
+import frappe
 from frappe.model.document import Document
 
-
 class FamilyMember(Document):
-	def validate(self):
+    def validate(self):
         if self.telegram_id:
             if not self.telegram_id.isdigit() or not (9 <= len(self.telegram_id) <= 12):
                 frappe.throw("Telegram ID must be a numeric value between 9 and 12 digits.")
