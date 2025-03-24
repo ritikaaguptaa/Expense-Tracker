594d979c2e1cf07989b42cfbb1eb2826d8a2c076
ritikaaguptaa <ritika@korecent.com>
2025-03-24 11:18:10 +0530

    Add Custom Button & Dialog Box

--- expense_tracker/api.py
+++ expense_tracker/api.py
@@ -1,25 +1,25 @@
-import frappe
+# import frappe
 
-@frappe.whitelist()
-def get_expense_data():
-    data = frappe.db.sql("""
-        SELECT category, SUM(amount) as total FROM `tabExpenses`
-        GROUP BY category
-    """, as_dict=True)
+# @frappe.whitelist()
+# def get_expense_data():
+#     data = frappe.db.sql("""
+#         SELECT category, SUM(amount) as total FROM `tabExpenses`
+#         GROUP BY category
+#     """, as_dict=True)
 
-    category_labels = [d['category'] for d in data]
-    category_values = [d['total'] for d in data]
+#     category_labels = [d['category'] for d in data]
+#     category_values = [d['total'] for d in data]
 
-    total_expenses = sum(category_values)
-    top_category = category_labels[category_values.index(max(category_values))] if category_labels else "-"
-    total_transactions = frappe.db.count("Expenses")
+#     total_expenses = sum(category_values)
+#     top_category = category_labels[category_values.index(max(category_values))] if category_labels else "-"
+#     total_transactions = frappe.db.count("Expenses")
 
-    return {
-        "total_expenses": total_expenses,
-        "top_category": top_category,
-        "total_transactions": total_transactions,
-        "category_data": {
-            "labels": category_labels,
-            "values": category_values
-        }
-    }
+#     return {
+#         "total_expenses": total_expenses,
+#         "top_category": top_category,
+#         "total_transactions": total_transactions,
+#         "category_data": {
+#             "labels": category_labels,
+#             "values": category_values
+#         }
+#     }
